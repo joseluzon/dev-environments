ff7ef35796005890cf175a66d23c7d44a929f15c
jluzonm <jluzonm@indra.es>
2022-08-30 11:25:32 +0200

    topics init script.

---
+++ compose-main/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic tracks-topic --config retention.ms=100 log.retention.ms=100
+kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic connector-info-topic --config retention.ms=100 log.retention.ms=100
+kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic plots-topic --config retention.ms=100 log.retention.ms=100
+kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic strobes-topic --config retention.ms=100 log.retention.ms=100
+kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic positions-topic --config retention.ms=100 log.retention.ms=100
+kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic ranges-topic --config retention.ms=100 log.retention.ms=100
+kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic northmarkers-topic --config retention.ms=100 log.retention.ms=100
+kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic scansectors-topic --config retention.ms=100 log.retention.ms=100
+kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic scanperiods-topic --config retention.ms=100 log.retention.ms=100
+kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic alerts-topic --config retention.ms=100 log.retention.ms=100
+kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic failures-topic --config retention.ms=100 log.retention.ms=100
+kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic lrus-topic --config retention.ms=100 log.retention.ms=100
+kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic datarequests-topic --config retention.ms=100 log.retention.ms=100
