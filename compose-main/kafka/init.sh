e6860b3acec74a0e73b83710c5754ea677f8a5f3
jluzonm <jluzonm@indra.es>
2022-11-02 10:53:01 +0100

    kafka without zookeeper and kafdrop

--- compose-main/kafka/init.sh
+++ compose-main/kafka/init.sh
@@ -16,3 +16,9 @@ kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replic
 kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic commandssend-topic --config retention.ms=100 log.retention.ms=100
 kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic datacommand-topic --config retention.ms=100 log.retention.ms=100
 kafka-topics --create --if-not-exists --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic accessrequest-topic --config retention.ms=100 log.retention.ms=100
+
+
+# kafka-topics --bootstrap-server=localhost:9092 --list
+# kafka-consumer-groups --list --bootstrap-server localhost:9092
+# kafka-consumer-groups --bootstrap-server localhost:9092 --all-groups --describe
+# watch -n 10 'kafka-consumer-groups --bootstrap-server localhost:9092 --all-groups --describe'
